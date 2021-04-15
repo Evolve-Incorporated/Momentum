fbe2f22467e13a31e5c8e39eadc981b2ebb48b61
Wielqus <wielqus@gmail.com>
2021-04-15 21:11:53 +0200

    Add OpenStreetView to the app

--- momentum/lib/homePage.dart
+++ momentum/lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key, this.title}) : super(key: key);
@@ -10,22 +11,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Hello World',
-            ),
-          ],
+      body: FlutterMap(
+        options: MapOptions(
+
         ),
-      ),
+        layers: [
+          TileLayerOptions(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: ['a', 'b', 'c']
+          ),
+          MarkerLayerOptions(
+            markers: [
+              Marker(
+                width: 80.0,
+                height: 80.0,
+                builder: (ctx) => Container(
+                  child: FlutterLogo(),
+                )
+              )
+            ]
+          )
+        ],
+      )
     );
   }
 }
